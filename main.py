0188f795b621411e7b374bb253bfb5e8292c93b1
micandly <80186523+micandly@users.noreply.github.com>
2023-04-08 17:13:07 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+import streamlit as st
+# import numpy as np
+import pandas as pd
+
+# 菜单栏
+from st_on_hover_tabs import on_hover_tabs
+
+st.set_page_config(layout="wide")
+
+st.header("老子")
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+with st.sidebar:
+    tabs = on_hover_tabs(tabName=['地图可视化', '知识图谱', '时间轴'], 
+                          iconName=['a', 'b', 'c'], styles = {'navtab': {'background-color':'rgb(240, 242, 246)',
+                                                  'color': '#818181',
+                                                  'font-size': '18px',
+                                                  'transition': '.3s',
+                                                  'white-space': 'nowrap',
+                                                  'text-transform': 'uppercase'},
+                                       'tabOptionsStyle': {':hover :hover': {'color': 'red',
+                                                                      'cursor': 'pointer'}},
+                                       'iconStyle':{'position':'fixed',
+                                                    'left':'7.5px',
+                                                    'text-align': 'left'},
+                                       'tabStyle' : {'list-style-type': 'none',
+                                                     'margin-bottom': '30px',
+                                                     'padding-left': '30px'}},
+                             key="1")
+
+if tabs =='地图可视化':
+    st.title("地图可视化")
+    import map_pydeck
+    
+elif tabs == '知识图谱':
+    st.title("知识图谱")
+    import graph
+    
+    
+elif tabs == '时间轴':
+    st.title("时间轴")
+    import timeline
+# from streamlit_option_menu import option_menu
+# selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+#     icons=['house', 'cloud-upload', "list-task", 'gear'], 
+#     menu_icon="cast", default_index=0, orientation="horizontal")
+
+
+
+
+
+
+    
---
+++ timeline.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 14:47:20 2023
+
+@author: admin
+"""
+
+from streamlit_timeline import timeline
+
+
+# use full page width
+# st.set_page_config(page_title="Timeline Example", layout="wide")
+
+# load data
+with open('E:\ECNU\streamlit\example.json', "r") as f:
+    data = f.read()
+
+# render timeline
+timeline(data, height=800)
