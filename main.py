5024e240dd6a8e53b02435150c13996154eabc65
micandly <80186523+micandly@users.noreply.github.com>
2023-04-10 17:51:43 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+# from st_on_hover_tabs import on_hover_tabs
+# import streamlit as st
+# st.set_page_config(layout="wide")
+
+# st.header("Custom tab component for on-hover navigation bar")
+# st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True)
+
+
+# with st.sidebar:
+#     tabs = on_hover_tabs(tabName=['Dashboard', 'Money', 'Economy'], 
+#                          iconName=['dashboard', 'money', 'economy'], default_choice=0)
+
+# if tabs =='Dashboard':
+#     st.title("Navigation Bar")
+#     st.write('Name of option is {}'.format(tabs))
+
+# elif tabs == 'Money':
+#     st.title("Paper")
+#     st.write('Name of option is {}'.format(tabs))
+
+# elif tabs == 'Economy':
+#     st.title("Tom")
+#     st.write('Name of option is {}'.format(tabs))
+
+
+
+
 import streamlit as st
 # import numpy as np
 import pandas as pd
@@ -20,27 +47,29 @@ with st.sidebar:
                                                   'transition': '.3s',
                                                   'white-space': 'nowrap',
                                                   'text-transform': 'uppercase'},
-                                       'tabOptionsStyle': {':hover :hover': {'color': 'red',
+                                        'tabOptionsStyle': {':hover :hover': {'color': 'red',
                                                                       'cursor': 'pointer'}},
-                                       'iconStyle':{'position':'fixed',
+                                        'iconStyle':{'position':'fixed',
                                                     'left':'7.5px',
                                                     'text-align': 'left'},
-                                       'tabStyle' : {'list-style-type': 'none',
-                                                     'margin-bottom': '30px',
-                                                     'padding-left': '30px'}},
-                             key="1")
+                                        'tabStyle' : {'list-style-type': 'none',
+                                                      'margin-bottom': '30px',
+                                                      'padding-left': '30px'}},
+                              key="1")
 
 TabName = ['地图可视化', '知识图谱', '时间轴']
 
-if tabs =='地图可视化':
-    st.title("地图可视化")
-    import map_pydeck
-elif tabs == '知识图谱':
-    st.title("知识图谱")
-    import graph  
-elif tabs == '时间轴':
-    st.title("时间轴")
-    import timeline
+if __name__=="__main__":
+
+    if tabs =='地图可视化':
+        st.title("地图可视化")
+        import map_pydeck
+    elif tabs == '知识图谱':
+        st.title("知识图谱")
+        import graph  
+    elif tabs == '时间轴':
+        st.title("时间轴")
+        import timeline
 # from streamlit_option_menu import option_menu
 # selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
 #     icons=['house', 'cloud-upload', "list-task", 'gear'], 
